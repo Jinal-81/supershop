b5e5b8db1e1b8fcbafd494be7fc1558d1f2af6e9
jinal <jinal.desai@trootech.com>
2022-03-28 13:31:00 +0530

    Mar 28 commit with admin panel change

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from cart.models import Cart, CartItem
 # Register your models here.
-admin.site.register(Cart)
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'total_amount', 'status')
+    list_filter = ('total_amount', 'status')
+
+
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem)
